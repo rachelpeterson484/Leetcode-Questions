86ba482e1c650491e73e2e8d205847953cb458cc
alexp0804 <alexpeterson0804@gmail.com>
2025-06-02 21:55:12 -0700

    q8 alex

---
+++ arrays_and_strings/8-alex-best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy_price = prices[0]
+        profit = 0
+
+        for i in range(len(prices) - 1):
+            diff = prices[i+1] - prices[i]
+            if diff > 0:
+                profit += diff
+
+        return profit
+
